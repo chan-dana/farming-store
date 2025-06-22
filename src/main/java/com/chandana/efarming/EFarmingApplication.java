a2e7b28eac62c74f1101714a6407903927d9b4cd
Chandana N <nchandana200@gmail.com>
2025-06-22 14:15:40 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/chandana/efarming/EFarmingApplication.java
@@ -0,0 +1,13 @@
+package com.chandana.efarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFarmingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFarmingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chandana/efarming/EFarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandana.efarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFarmingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
